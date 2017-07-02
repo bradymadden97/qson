f3e5a6b15740a6e490ebd85f4b9b7b4601115c63
Brady Madden <bradymadden97@gmail.com>
2017-07-02 01:19:36 -0400

    init materialize/ ui

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
 //Consts and imports
 const express = require('express');
+const exphb = 	require('express-handlebars');
 const path = require('path');
 const spawn = require("child_process").spawn;
 const app = express();
 
+//Handlebars
+var hbs = exphb.create({
+	defaultLayout: 'main',
+	helpers: {
+		section: function(name, options){
+			if(!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+		}
+	},
+	partialsDir: __dirname + '/views/partials/'
+});
+
 //Setup
 app.use(express.static(__dirname + '/public'));
+app.use('/css', express.static(__dirname + '/node_modules/materialize-css/dist/css'))
+app.use('/js', express.static(__dirname + '/node_modules/materialize-css/dist/js'))
+app.use('/js', express.static(__dirname + '/node_modules/materialize-css/node_modules/jquery/dist'))
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //Route functions
 function index(req, res){
+    res.render('index', {
+
+    });
+};
+function parse(req, res){
     var process =  spawn('python', ["qson.py", "-i", "test/test.txt", "-w"]);
     process.stdout.on('data', function (data) {
         res.send(JSON.parse(data));
     });
-}
+};
 
 //Routes
 app.get("/", function(req, res){
-	index(req, res);
+    index(req, res);
+});
+app.post("/parse", function(req, res){
+	parse(req, res);
 });
 
 
