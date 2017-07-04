bd7b3b022ff126af5b8254088d42285106ba90d3
Brady Madden <bradymadden97@gmail.com>
2017-07-04 19:54:55 -0400

    downloading works

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphb = 	require('express-handlebars');
 const path = require('path');
 const spawn = require("child_process").spawn;
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
 //Handlebars
@@ -43,6 +44,17 @@ function parse(req, res){
     process.stdin.write(JSON.stringify(req.body.data));
 	process.stdin.end();
 };
+function download(req, res){
+	var data = req.body.output;
+	fs.writeFile('output.json', data, 'utf8', function(err){
+		if(err) throw err;
+		res.download('output.json', function(err){
+			fs.unlink('output.json', function(err){
+				if(err) throw err;
+			});
+		});
+	});
+};
 
 //Routes
 app.get("/", function(req, res){
@@ -50,6 +62,15 @@ app.get("/", function(req, res){
 });
 app.post("/parse", function(req, res){
 	parse(req, res);
+});
+app.get("/parse", function(req, res){
+	res.redirect("/");
+});
+app.post("/download", function(req, res){
+	download(req, res);
+});
+app.get("/download", function(req, res){
+	res.redirect("/");
 });
 
 
